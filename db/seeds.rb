4ec39ccb92be8baa529290470f6cb268984eeb6d
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-28 12:31:57 -0800

    got seed bikes appearing on the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bike.create({name: "Soma Rush", description: "Green steel fixed gear"})
+Bike.create({name: "Dolan Carbon", description: "Black carbon fixed gear magic"})
