a47c95a1ed2111171289b6af7dbb2601c08e532a
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-29 11:30:30 -0800

    auth mostly working but cannot create new user - no password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+		has_secure_password
 	has_many :bikes
-	has_secure_password
-	validates_presence_of :email, :notice => "Need an email"
-	validates_uniqueness_of :email, :notice => "Email is already taken"
-	validates_presence_of :name, :notice => "Need a name"
-	validates_uniqueness_of :name, :notice => "Name is already taken"
+		validates :name, presence: true
+		validates :email,
+			presence: true,
+			uniqueness: true
+		validates :password, length: {minimum: 6}, on: :create 
 end
