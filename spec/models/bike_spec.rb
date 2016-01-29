277f937d507767b46d8813671f292c00a7211900
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-29 12:26:29 -0800

    got a bike model testing working

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Bike, type: :model do
 
-	# before do
-	# 	bike_params = Hash.new
-	# 	bike_params[:name] = FFaker::Name.first_name
-	# 	bike_params[:description] = FFaker::Baconlpsum.phrase
-	# 	@bike = Bike.create(bike_params)
+	before do
+    bike_params = Hash.new
+    bike_params[:name] = FFaker::Name.first_name
+    bike_params[:description] = FFaker::BaconIpsum.phrase
 
- #  	pending "add some examples to (or delete) #{__FILE__}"
+    @bike = Bike.create(bike_params)
+  end
 
-  # describe "name" do
-  # 	it "provides a name" do
-  # 		expect(@bike.name).to eq(true)
-  # 	end
-  # end
+	describe "test validation" do
+		it "validates that a bike has a name and description" do
+			expect(@bike.name).to eq("#{@bike.name}")
+		end
+	end
 
 end
