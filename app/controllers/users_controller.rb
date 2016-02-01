412ed4eb9221584e2fe839d8c5a7c933b37b7b32
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-31 16:01:05 -0800

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,56 @@
 class UsersController < ApplicationController
 
-	def new
+	before_filter :authorize, only: [:edit, :update, :destroy]
+
+	# GET /users
+	def index
+		@users = User.all
 	end
 
+	# GET /users/id
+
+	def show
+		@user = User.find(params[:id])
+		@bikes = @user.bikes
+	end
+		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+		@user = current_user
+	end
+
+	# POST / users
 	def create
 		user = User.new(user_params)
+		puts user
 		if user.save
 			session[:user_id] = user.id 
 			redirect_to '/'
 		else
+			flash[:notice] = user.errors.map{ |k,v| "#{k} #{v}".capitalize }
 			redirect_to '/signup'
+		end
+	end
+
+	def update
+		@user = current_user
+		user_params = params.require(:user).permit(:name, :email)
+		puts user_params
+		puts @user.update_attributes(user_params)
+		if @user.update_attributes(user_params)
+			redirect_to edit_user_path
+		end
+	end
+
+	def destroy
+		@user.destroy
+		respond_to do |format|
+			format.html { redirect_to users_url, notice: "User was successfully deleted." }
 		end
 	end
 
