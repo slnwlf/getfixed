54272a277700daaabda40e4f35a4a5cfef4c3dc9
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-02-03 15:50:58 -0800

    got flash message to fade away

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+def flash_messages(opts = {})
+  flash.each do |msg_type, message|
+    concat(content_tag(:div, message, class: "alert #{flash_type.to_sym(msg_type)} fade in") do 
+            concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+            concat content_tag(:i, nil, class: "glyphicon glyphicon-#{bootstrap_icon_for(type)}")
+            concat message 
+          end)
+  end
 end
+ 
+  nil
+end
+
