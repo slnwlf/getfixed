c1a04ff562cfec0ff925fd0bf84eae9b9b5b4426
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-02-02 16:21:33 -0800

    trying adding :modules

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,6 @@
 class Bike < ActiveRecord::Base
 	extend FriendlyId
-	friendly_id :name, use: :slugged
+	friendly_id :name, use: [:slugged, :finders]
 
 	def should_generate_new_friendly_id?
 		new_record?
