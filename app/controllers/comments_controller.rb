dc4f7250b375a3d89c2c27a3808df33c259465ab
tkhuynh <thequest2311@yahoo.com>
2016-02-08 08:57:28 -0800

    edit comment, user delete it own comment, bike owner can delete any comments tied to his bike

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,34 @@ class CommentsController < ApplicationController
   end
 
   def edit
-  
+    unless current_user == @comment.user
+      redirect_to bike_path(@bike)
+    end
+  end
+
+  def update
+    if current_user == @comment.user
+      if @comment.update_attributes(comment_params)
+        flash[:notice] = "Successfully updated comment."
+        redirect_to bike_path(@bike)
+      else
+        flash[:error] = @comment.errors.full_messages.join(", ")
+        redirect_to edit_bike_comment_path(@bike, @comment)
+      end
+    else
+      redirect_to bike_path(@bike)
+    end
+  end
+
+  def destroy
+    #owner of bike and delete any comments tied to his bike
+    if current_user == @bike.user or @bike.id == @comment.bike.id 
+      @comment.destroy
+      flash[:notice] = "Successfully deleted comment."
+      redirect_to bike_path(@bike)
+    else
+      redirect_to bike_path(@bike)
+    end
   end
 
 private
