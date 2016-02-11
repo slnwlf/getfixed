8071a413dd040e3c95b311c6b8c9bb46d7c88c2a
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-02-11 15:25:14 -0800

    got ENV setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: 'fixedgearnation.contact',
-    password: '44Soma16',
+    user_name: ENV['EMAIL_USERNAME'],
+    password: ENV['EMAIL_PASSWORD'],
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: 587,
