562e7c8a323588af50845036c30b36e407268058
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-02-11 20:08:42 -0800

    fixed typo on user_mailer and tuned up welcome view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
-	default from: "fixedgearnation.contact@gmail.com
-	"
+	default from: "fixedgearnation.contact@gmail.com"
+	
 	def welcome(user)
 		@user = user
 		mail( to: @user.email, subject: "Thanks for signing up for Fixed Gear Nation!")
