65ee913f3e624ba8d1bf5d1ca716e3938dd8f3ca
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-02-23 08:33:30 -0800

    validate the image size is less than 1 mb

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -20,4 +20,6 @@ class Bike < ActiveRecord::Base
 
 	has_attached_file :image, styles: { xl: "800*800>",large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+  
 end
