38e7401401c499d110875b9ec9f2d74bf428dfbb
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-03-15 12:34:59 -0700

    trying update the new_record

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	friendly_id :name, use: [:slugged, :finders]
 
 	def should_generate_new_friendly_id?
-		new_record?
+		new_record? || slug.blank?
 	end
 
 	has_secure_password
