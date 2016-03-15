07d75c7b75749917cdff8055f8d8bb3bdaef28cf
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-03-15 12:37:48 -0700

    commenting out code again

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: [:slugged, :finders]
 
-	def should_generate_new_friendly_id?
-		new_record? || slug.blank?
-	end
+	# def should_generate_new_friendly_id?
+	# 	new_record? || slug.blank?
+	# end
 
 	has_secure_password
 	has_many :bikes, dependent: :destroy
