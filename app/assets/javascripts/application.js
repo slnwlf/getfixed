3c53af32a0860868fab892d730fdfe4052eb07dd
tkhuynh <thequest2311@yahoo.com>
2016-03-17 14:39:03 -0700

    add elevate zoom for additional photos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,11 +31,70 @@ $(function() {
 });
 
 $(function() {
-    if ($(window).width() < 600) {
-        $(".bike-pic-show").elevateZoom({constrainType:"height", constrainSize:274, zoomType: "lens", containLensZoom: true, gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: "active"});
-    } else {
-        $(".bike-pic-show").elevateZoom({zoomWindowPosition: 1, zoomWindowOffetx: 10});
+
+    function additional_photo_zoom() {
+        $('body').on('click', '.additional-photo-holder', function() {
+            // get value on clicked image
+            var this_addtional_photo = $(this).children();
+            var this_addtional_photo_thumb_url = this_addtional_photo.attr("src");
+            // change to large size, add data zoom and swap class
+            this_addtional_photo_large_url = this_addtional_photo_thumb_url.replace(/thumb/, "large");
+            this_addtional_photo_xl_url = this_addtional_photo_thumb_url.replace(/thumb/, "xl");
+            this_addtional_photo = this_addtional_photo.attr({
+                src: this_addtional_photo_large_url,
+                "data-zoom-image": this_addtional_photo_xl_url
+            }).removeClass("individual-additional-photo").addClass('bike-pic-show')[0];
+            // get value on main-photo
+            var main_photo = $('.main-photo-holder').children();
+            var main_photo_url = main_photo.attr("src");
+            // change to thumb size and swap class
+            main_photo_url = main_photo_url.replace(/large/, "thumb");
+            main_photo = main_photo.attr("src", main_photo_url).removeClass("bike-pic-show").addClass("individual-additional-photo")[0];
+            // swap photo 
+            $('.main-photo-holder').html(this_addtional_photo);
+            $(this).html(main_photo);
+            $('#zoomed img').removeData('elevateZoom');
+            $('.zoomWrapper img.zoomed').unwrap();
+            $('.zoomContainer').remove();
+            if ($(window).width() < 600) {
+                $(".bike-pic-show").elevateZoom({
+                    constrainType: "height",
+                    constrainSize: 274,
+                    zoomType: "lens",
+                    containLensZoom: true,
+                    gallery: 'gallery_01',
+                    cursor: 'pointer',
+                    galleryActiveClass: "active"
+                });
+            } else {
+                $(".bike-pic-show").elevateZoom({
+                    zoomWindowPosition: 1,
+                    zoomWindowOffetx: 10
+                });
+            }
+        });
     }
+
+    // set zoom for main photo
+    if ($(window).width() < 770) {
+        $(".bike-pic-show").elevateZoom({
+            constrainType: "height",
+            constrainSize: 274,
+            zoomType: "lens",
+            containLensZoom: true,
+            gallery: 'gallery_01',
+            cursor: 'pointer',
+            galleryActiveClass: "active"
+        });
+    } else {
+        $(".bike-pic-show").elevateZoom({
+            zoomWindowPosition: 1,
+            zoomWindowOffetx: 10
+        });
+    }
+    // set zoom for additional photo when user click on it
+    additional_photo_zoom();
+
     function onGeocomplete() {
         var options = {
             types: ['(cities)'],
